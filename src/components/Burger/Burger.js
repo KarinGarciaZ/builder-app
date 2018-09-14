c60a2d1d79a825397c91dc0dd59afb2e1b3067bd
KarinGarciaZ <oscarin962011@hotmail.com>
2018-09-13 22:46:40 -0500

    Ingredients are called dynamically.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,27 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map( ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map( (_,i) => {
+        return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+      })
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+
+  if (!transformedIngredients.length){
+    transformedIngredients = <p>Please add ingredients.</p>
+  }
+
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="bread-bottom" />
+      {
+        transformedIngredients
+      }
+        <BurgerIngredient type="bread-bottom" />
     </div>
   );
 }

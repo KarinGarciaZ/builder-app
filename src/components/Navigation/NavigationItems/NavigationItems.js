dacdf2eb87b1f226ce8445f42f749fcb7de145a0
KarinGarciaZ <oscarin962011@hotmail.com>
2018-09-15 13:43:38 -0500

    Making Toolbar responsive. Added a sidedrawer.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
+import classes from  './NavigationItems.css';
 
 const navigationItems = (props) => (
-  <ul className={classes.navigationItems}>
+  <ul className={classes.NavigationItems}>
     <NavigationItem link="/" active={true}>Burger Builder</NavigationItem>
     <NavigationItem link="/">Checkout</NavigationItem>
   </ul>

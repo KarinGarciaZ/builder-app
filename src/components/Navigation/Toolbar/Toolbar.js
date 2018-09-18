376fff8b23218a5dee1b8993db1604c05912c023
KarinGarciaZ <oscarin962011@hotmail.com>
2018-09-17 21:37:45 -0500

    Implemented a sidedrawer to toggle menu in phone view.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle toggleClick={props.toggleClicked}>MENU</DrawerToggle>
     <div className={classes.Logo}><Logo /></div>
     <nav className={classes.DesktopOnly}><NavigationItems /></nav>
   </header>

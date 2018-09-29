8a389d11851811e1b9f53a06487a0295b13da9ee
KarinGarciaZ <oscarin962011@hotmail.com>
2018-09-29 14:14:59 -0500

    Added instances of routing to components.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Aux from './hoc/Aux'
+import Checkout from './containers/Checkout/Checkout';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Aux>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </Aux>
+      <BrowserRouter>
+        <Aux>
+          <Layout>
+            <Switch>
+              <Route path="/checkout" component={Checkout}/>
+              <Route path="/" component={BurgerBuilder}/>
+            </Switch>
+          </Layout>
+        </Aux>      
+      </BrowserRouter>
     );
   }
 }

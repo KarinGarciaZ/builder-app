1755b2f58648f5c18ce2ba7466907376e2867ff6
KarinGarciaZ <oscarin962011@hotmail.com>
2018-10-10 22:15:08 -0500

    Added validation to form.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,29 +8,51 @@ import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component {
   state = {
     customerInfo: {
+
       customer: {
         elementType: 'input',
+        valid: false,
+        touched: false, 
+        value: '',
         elementConfig: {
           type: 'text',
           placeholder: 'Your Name'
-        }, 
-        value: ''
+        },
+        validation: {
+          required: true,
+          maxLength: 100
+        }
       },
+
       address: {
         elementType: 'input',
+        valid: false,
+        touched: false, 
+        value: '',
         elementConfig: {
           type: 'text',
           placeholder: 'Your address'
-        }, 
-        value: ''
+        },
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 150
+        }
       },
+
       email: {
         elementType: 'input',
+        value: '',
+        valid: false,
+        touched: false,
         elementConfig: {
-          type: 'text',
+          type: 'email',
           placeholder: 'Your mail'
         }, 
-        value: ''
+        validation: {
+          mailValidation: true,
+          maxLength: 100
+        }
       },
     },    
     ingredients: {},
@@ -54,9 +76,9 @@ class ContactData extends Component {
        cheese: this.state.ingredients.cheese,
        meat: this.state.ingredients.meat,
        total_price: this.state.total_price,
-       customer: 'Karin',
-       address: 'Marcos Gordoa 51',
-       email: 'karin@gmail.com'
+       customer: this.state.customerInfo.customer.value,
+       address: this.state.customerInfo.address.value,
+       email: this.state.customerInfo.email.value
      }
      axios.post('/order/', order)
        .then( responce => {
@@ -70,6 +92,46 @@ class ContactData extends Component {
        })
   }
 
+  checkValidity(value, rules) {
+    let isValid = [];
+
+    if( rules.required )
+      isValid.push(value.trim() !== '');
+
+    if( rules.minLength )
+      isValid.push(value.trim().length >= rules.minLength);
+
+    if( rules.maxLength )
+      isValid.push(value.trim().length <= rules.maxLength);
+
+    if( rules.mailValidation ) 
+      if ( value.includes('@') ) {
+        let arrMail = value.split('@')
+        if ( arrMail.length <= 2 ) {
+          if ( arrMail[0].length > 0 && arrMail[1].length > 2 ) {
+            if ( arrMail[1].includes('.') )
+              isValid.push(true); 
+            else isValid.push(false);
+          } else isValid.push(false);    
+        } else isValid.push(false);
+      } else isValid.push(false);
+
+    for( let bool of isValid )
+      if ( !bool ) return false;
+    return true;
+    
+  }
+
+  changedValueInput = ( id, event ) => {
+    let customer = { ...this.state.customerInfo }
+    let values = { ...customer[id] }
+    values.value = event.target.value;
+    values.valid = this.checkValidity( values.value, values.validation )
+    values.touched = true
+    customer[id] = values;
+    this.setState({ customerInfo: customer })
+  }
+
   render () {
     
     let formElementArray = [];
@@ -80,16 +142,25 @@ class ContactData extends Component {
       })
     }
 
-    let form = (<form>      
+    let form = (<form onSubmit={this.makeOrder}>      
       {formElementArray.map( formElement => {
-        return <Input key={formElement.id} inputtype={formElement.config.elementType} elementConfig={formElement.config.elementConfig} value={formElement.config.value} name="customer" placeholder="Your name" />
+        return <Input 
+          key={formElement.id} 
+          inputtype={formElement.config.elementType} 
+          elementConfig={formElement.config.elementConfig} 
+          value={formElement.config.value} 
+          changed={( event ) => this.changedValueInput(formElement.id, event)}
+          valid={formElement.config.valid}
+          shouldValidate={formElement.config.validation}
+          touched={formElement.config.touched}
+          />
       })}
-      <Button btnType="SuccessButton" clicked={this.makeOrder} disabled >Order</Button>
+      <Button btnType="SuccessButton" disabled >Order</Button>
     </form>);
 
     if( this.state.loading )
       form = <Spinner />
-    return(
+    return(     
       <div className={classes.ContactData}>
         <h3>Enter your contact data!</h3>
         {form}

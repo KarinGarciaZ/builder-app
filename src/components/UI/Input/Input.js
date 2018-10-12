9194ca4c4305acbd74ed6ee045ce8b5c4183bb83
KarinGarciaZ <oscarin962011@hotmail.com>
2018-10-12 17:08:34 -0500

    New css classes added to project.

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,9 +4,12 @@ import classes from './Input.css';
 const input = ( props ) => {
   let imputElement = null;
   let inputClasses = [ classes.InputElement ]
+  let inputError = null;
 
-  if ( !props.valid && props.shouldValidate && props.touched )
+  if ( !props.valid && props.shouldValidate && props.touched ){
+    inputError = (<p className={classes.InputError}>Please enter a valid {props.elementConfig.placeholder}</p>)
     inputClasses.push(classes.Invalid);
+  }
   if ( props.valid && props.shouldValidate && props.touched ) 
     inputClasses.push(classes.Valid);
 
@@ -25,6 +28,7 @@ const input = ( props ) => {
     <div className={classes.Input}>
       <label className={classes.Label}>{props.label}</label>
       {imputElement}
+      {inputError}
     </div>
   );
 
